b68ef52e40a86b1df997e6b9baad3b4f9a369f6c
wei shi <weishi@weideMacBook-Pro.local>
2017-07-03 10:08:59 +0800

    Add tag v1.0

--- TFLive/TFMoviePlayViewController.m
+++ TFLive/TFMoviePlayViewController.m
@@ -27,8 +27,8 @@
     _livePlayer = [[TFLivePlayController alloc] initWithLiveURL:liveURL];
     [_livePlayer prepareToPlay];
     
-//    _livePlayer.playView.frame = CGRectMake(0, 100, 50, 100);
-    _livePlayer.playView.frame = self.view.bounds;
+    _livePlayer.playView.frame = CGRectMake(0, 100, 50, 100);
+    //_livePlayer.playView.frame = self.view.bounds;
     [self.view addSubview:_livePlayer.playView];
     
 }
